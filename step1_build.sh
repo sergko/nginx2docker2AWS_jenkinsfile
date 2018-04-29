f0d21cee09a37e8ce49f88ed26935fe710044eda
sergey kovbyk <kovbyk@gmail.com>
2018-04-29 20:23:23 +0300

    fixed $ngrelPATH

--- step1_build.sh
+++ step1_build.sh
@@ -108,4 +108,4 @@ checkinstall -D -y --install=no -d2 \
 #ln -sf nginx-opswork_1.14.0-${BUILD_NUMBER}_amd64.deb /tmp/nginx-opswork.deb
 #ln -sf nginx-opswork_1.14.0-${BUILD_NUMBER}_amd64.deb ../nginx-opswork.deb
 cd ..
-cp -f ./ngrelPATH/nginx-opswork_1.14.0-${BUILD_NUMBER}_amd64.deb nginx-opswork.deb
+cp -f ./$ngrelPATH/nginx-opswork_1.14.0-${BUILD_NUMBER}_amd64.deb nginx-opswork.deb
